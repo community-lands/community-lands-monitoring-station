042ddea9dcd947f43bb2bbd6a7e1c6cffe9d4cde
Rob Heittman <rob.heittman@solertium.com>
2015-11-13 23:12:26 -0500

    Push build directory down a level

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-rm -rf MonitoringStation-*
-electron-packager . MonitoringStation --platform=darwin --arch=x64 --version=0.33.1
-zip -r MonitoringStation-darwin-x64-1.0.0.zip MonitoringStation-darwin-x64
-electron-packager . MonitoringStation --platform=win32 --arch=x64 --version=0.33.1
-zip -r MonitoringStation-win32-x64-1.0.0.zip MonitoringStation-win32-x64
+rm -rf builds
+electron-packager . MonitoringStation --platform=darwin --arch=x64 --out builds --version=0.33.1
+# electron-packager . MonitoringStation --platform=win32 --arch=x64 --out builds --version=0.33.1
+cd builds
+# zip -r MonitoringStation-darwin-x64-1.0.0.zip MonitoringStation-darwin-x64
+# zip -r MonitoringStation-win32-x64-1.0.0.zip MonitoringStation-win32-x64
+cd ..
