585953a5aec4bd2d6d1e55a1d05e2adbefa9b02c
Rob Heittman <rob.heittman@solertium.com>
2015-11-24 12:11:21 -0500

    Fix a wrong comment character

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var Strategy = require('passport-http').DigestStrategy;
 var db = require('./db');
 
 var tile_cache = require('./helpers/tile-cache');
-# tile_cache.pyramid_urls(-76.3605,-9.4491,-72.757,-6.1078, 1, 13);
+// tile_cache.pyramid_urls(-76.3605,-9.4491,-72.757,-6.1078, 1, 13);
 
 var FormSubmissionMiddleware = require('openrosa-form-submission-middleware')
 var ProcessSubmission = require('./middlewares/process-submission')
