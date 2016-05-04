3277f120bf077817a3014ff997cf3c11acf26ba6
Rob Heittman <rob.heittman@solertium.com>
2016-05-03 21:27:38 -0400

    Remove global forms abstraction

--- helpers/community-storage.js
+++ helpers/community-storage.js
@@ -4,39 +4,28 @@ var fs = require('fs')
 var path = require('path')
 var mkdirp = require('mkdirp')
 
-const GLOBAL_FORMS = settings.getGlobalFormsDirectory()
 const GLOBAL_MAPS = settings.getGlobalMapsDirectory()
 
 function getFormUrls (cb) {
   var local_forms = settings.getUserFormsDirectory()
-  fs.readdir(GLOBAL_FORMS, function (err, global) {
-    fs.readdir(local_forms, function (err2, local) {
-      if (err) {
-        cb(err)
-      } else if (err2) {
-        cb(err2)
-      } else {
-        var files = global.concat(local)
-        var links = []
-        for (var i = 0; i < files.length; i++) {
-          links[i] = settings.getBaseUrl() + '/forms/' + files[i]
-        }
-        cb(err, links)
+  fs.readdir(local_forms, function (err, local) {
+    if (err) {
+      cb(err)
+    } else {
+      var files = local
+      var links = []
+      for (var i = 0; i < files.length; i++) {
+        links[i] = settings.getBaseUrl() + '/forms/' + files[i]
       }
-    })
+      cb(err, links)
+    }
   })
 }
 
 function getForm (file, cb) {
   var local_forms = settings.getUserFormsDirectory()
   fs.readFile(local_forms + '/' + file, function (err, data) {
-    if (err) {
-      fs.readFile(GLOBAL_FORMS + '/' + file, function (err2, data2) {
-        cb(err2, data2)
-      })
-    } else {
-      cb(err, data)
-    }
+    cb(err, data)
   })
 }
 
@@ -68,8 +57,9 @@ function getMap (file, cb) {
 
 function saveMap (file, data, cb) {
   var contents = data
-  if (typeof(data) != 'string')
+  if ((typeof data) !== 'string') {
     contents = JSON.stringify(data, null, 2)
+  }
   var mapFile = path.join(GLOBAL_MAPS, file)
   fs.writeFile(mapFile, contents, function (err) {
     cb(err)
