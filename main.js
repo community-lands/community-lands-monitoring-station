711792bc03791ca516c51fc504f2ec65f51ce949
Carl Scott <carl.scott@solertium.com>
2016-07-05 17:14:29 -0400

    Turn off dev tools.

--- main.js
+++ main.js
@@ -319,7 +319,7 @@ app.on('ready', function () {
   mainWindow.loadURL('file://' + __dirname + '/index.html?locale=' + (settings.getLocale() || 'en'))
 
   // Open the DevTools.
-  mainWindow.openDevTools()
+  // mainWindow.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
