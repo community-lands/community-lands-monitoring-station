d29b70a7fe96afef993b25f993d7242a979960bd
Rob Heittman <robheittman@imac.attlocal.net>
2016-07-05 12:55:13 -0400

    Update build scripts to use latest electron

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf builds
 # electron-packager . MonitoringStation --platform=darwin --arch=x64 --out builds --version=0.33.1
-electron-packager . MonitoringStation --platform=win32 --arch=x64 --out builds --version=0.37.3
+electron-packager . MonitoringStation --platform=win32 --arch=x64 --out builds --version=1.2.5
 cd builds
 # zip -r MonitoringStation-darwin-x64-1.0.0.zip MonitoringStation-darwin-x64
 cd MonitoringStation-win32-x64
