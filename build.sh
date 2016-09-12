985508709bc4860689735fbee2fc51db83f1ba98
Rob Heittman <robheittman@iMac-2.local>
2016-09-11 23:33:35 -0400

    Update electron-packager version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf builds
 # electron-packager . MonitoringStation --platform=darwin --arch=x64 --out builds --version=0.33.1
-electron-packager . MonitoringStation --platform=win32 --arch=x64 --out builds --version=1.2.5
+electron-packager . MonitoringStation --platform=win32 --arch=x64 --out builds --version=1.3.5
 cd builds
 # zip -r MonitoringStation-darwin-x64-1.0.0.zip MonitoringStation-darwin-x64
 cd MonitoringStation-win32-x64
