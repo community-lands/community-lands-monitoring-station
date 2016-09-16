1a2f7c3ac69d6901e5eb1b56c9cc8f43a722fe2d
Rob Heittman <rob.heittman@solertium.com>
2016-09-16 09:31:05 -0400

    electron to 1.4.0

--- build-mac.sh
+++ build-mac.sh
@@ -1,6 +1,6 @@
 rm -rf builds
 # electron-packager . MonitoringStation --platform=darwin --arch=x64 --out builds --version=0.33.1
-electron-packager `pwd` MonitoringStation --platform=darwin --arch=x64 --out builds --version=1.3.5
+electron-packager `pwd` MonitoringStation --platform=darwin --arch=x64 --out builds --version=1.4.0
 cd builds
 # zip -r MonitoringStation-darwin-x64-1.0.0.zip MonitoringStation-darwin-x64
 cd MonitoringStation-darwin-x64
